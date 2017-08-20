eb6d99b9d15cfa7c1a28e0db3a1f875b6e1b263d
Christopher Figueroa <christopher_figueroa@apple.com>
2017-08-20 14:39:13 -0700

    changing this a bit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # gpkg
-a package manager for games that links to repos
+a package manager for games that links to repos. Modeled after Bower, Yarn and Dep
+
+API:
+
+- help - outputs the commands
+- cache - the location of the download dependencies cache on your local machine
+- home - opens a package homepage if the URL isn't empty
+- info - displays the overall information of the package including version Number
+- init - creates a gpkg.json file
+- install - installs all dependencies listed in the gpkg.json file
+  - force-latest - force the latest version on conflict
+  - side - makes a folder with the newest version along side the old one to deal with manually
+- list - lists packages in the gpkg.json file and attempts to check if newer versions exist
+- login - authenticate with github and store credientials so we can use to register packages
+- register - registers a package with the database by checking to see if a gpkg exists with that name, if no, send gpkg.json file to server
+- update - updates all the installed packages to the newest version according to their version numbers in the gpkg.json file
+  - force-latest - force the latest version on conflict
+  - side - makes a folder with the newest version along side the old one to deal with manually
+- uninstall - uninstalls a package from the gpkg.json file and the location its installed
+- unregister - unregisters a package with the database
+- version - checks the gpkg version
--- app.js
+++ app.js
@@ -1,11 +1,63 @@
+#!/usr/bin/env node
+
+
+///Module dependencies. ///////////////////////////////////////
+
 var http = require('http');
 var fs = require('fs');
 var shell = require("shelljs");
 
-function CMD(command) {
-  shell.exec("git push origin master --force");
+/////////////////////////////////////////////////////////////////////
+
+var locationOfProcess;
+
+//check to see if we should process anything
+if(process.argv.length > 2)
+{
+  //set locationOfProcess
+  locationOfProcess = process.argv[2];
+
+  console.log(process.argv);
+}
+else {
+  //run help command
 }
 
+
+
+//
+// process.argv.forEach(function (val, index, array) {
+//   // console.log(index + ': ' + val);
+//   if(index >= 2)
+//   {
+//       console.log(index, val);
+//   }
+//
+//
+// });
+
+/////////////////////////////////////////////////////////////////////
+
+//fun the command given
+function CMD(command) {
+  //example:  shell.exec("git push origin master --force");
+  shell.exec(command);
+}
+
+function test {
+      var child = exec('some_long_running_process', {async:true});
+    child.stdout.on('data', function(data) {
+      /* ... do something with data ... */
+    });
+
+    exec('some_long_running_process', function(code, stdout, stderr) {
+      console.log('Exit code:', code);
+      console.log('Program output:', stdout);
+      console.log('Program stderr:', stderr);
+    });
+}
+
+//download a file from the given url
 function download(url, dest, cb) {
   var file = fs.createWriteStream(dest);
   var request = http.get(url, function(response) {
